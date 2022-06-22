8beac05bb4bb56eaf800bb6732ab6970b04d759d
Suditi04 <93833587+Suditi04@users.noreply.github.com>
2022-06-22 13:29:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event)
+{
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    if(content=="take my selfie");
+    {
+        conso0le.log("taking selfie ---");
+        speak();
+    }
+
+
+    document.getElementById("textbox").innerHTML = Content;
+    speak();
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = "taking your selfie in 5 seconds";    
+
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:"png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
+
+function take_snapshot() {
+
+Webcam.snap(function(data_url){
+    document.getElementById("result").innerHTML = '<img id=selfie_image" src ="' + data_url+'">';
+
+});
+
+}
+
+function setTimeOutButton() {
+
+    setTimeout(
+        function(){
+            alert("set TimeOut Button");
+        }, 3000);
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src
+    link.href = image;
+    link.click();
+}
+
